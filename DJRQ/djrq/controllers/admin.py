91780c8ba887e6f3cb58913295789b2e0a6c181b
Brian Millham <brian@millham.net>
2015-07-16 18:10:37 -0400

    Added branch for authentication

--- DJRQ/djrq/controllers/admin.py
+++ DJRQ/djrq/controllers/admin.py
@@ -6,8 +6,9 @@ from datetime import datetime
 from djrq.model import *
 from sqlalchemy.sql import func, or_
 from time import time
+from account import AccountMixIn
 
-class Admin(web.core.Controller):
+class Admin(web.core.Controller, AccountMixIn):
     #def __init__(self, id):
     #    print "Starting admin"
     #    super(Resource, self).__init__()
--- DJRQ/djrq/model/__init__.py
+++ DJRQ/djrq/model/__init__.py
@@ -9,6 +9,8 @@ from time import time
 import markupsafe
 import paste
 from web.core import request
+from sqlalchemy.ext.associationproxy import association_proxy
+#from auth import *
 
 Base = declarative_base()
 metadata = Base.metadata
@@ -19,6 +21,7 @@ from album import Album
 from played import Played
 from requestlist import RequestList
 from song import Song"""
+#__all__ = ['Base']
 
 class Album(Base):
     __tablename__ = 'album'
@@ -172,6 +175,97 @@ class Listeners(Base):
     current = Column(Integer)
     max = Column(Integer)
 
+class Account(Base):
+    __tablename__ = 'accounts'
+    __repr__ = lambda self: "Account(%s, '%s')" % (self.id, self.name)
+
+    id = Column(String(32), primary_key=True)
+    name = Column(Unicode(255), nullable=False)
+    _password = Column('password', String(128))
+
+    def _set_password(self, value):
+        if value is None:
+            self._password = None
+            return
+
+        import hashlib
+        encoder = hashlib.new('sha512')
+        encoder.update(value)
+        self._password = encoder.hexdigest()
+
+    password = synonym('_password', descriptor=property(lambda self: self._password, _set_password))
+
+    groups = association_proxy('_groups', 'id')
+
+    @property
+    def permissions(self):
+        perms = []
+
+        for group in self._groups:
+            for perm in group.permissions:
+                perms.append(perm)
+
+        return set(perms)
+
+    @classmethod
+    def authenticate(cls, identifier, password=None, force=False):
+        if not force and not password:
+            return None
+
+        try:
+            user = cls.get(identifier)
+
+        except:
+            return None
+
+        if force:
+            return user.id, user
+
+        import hashlib
+        encoder = hashlib.new('sha512')
+        encoder.update(password)
+
+        if user.password is None or user.password != encoder.hexdigest():
+            return None
+
+        return user.id, user
+
+
+account_groups = Table('account_groups', Base.metadata,
+                    Column('account_id', String(32), ForeignKey('accounts.id')),
+                    Column('group_id', Unicode(32), ForeignKey('groups.id'))
+            )
+
+
+class Group(Base):
+    __tablename__ = 'groups'
+    __repr__ = lambda self: "Group(%s, %r)" % (self.id, self.name)
+    __str__ = lambda self: str(self.id)
+    __unicode__ = lambda self: self.id
+
+    id = Column(String(32), primary_key=True)
+    description = Column(Unicode(255))
+
+    members = relation(Account, secondary=account_groups, backref='_groups')
+    permissions = association_proxy('_permissions', 'id')
+
+
+group_permissions = Table('group_perms', Base.metadata,
+                    Column('group_id', Unicode(32), ForeignKey('groups.id')),
+                    Column('permission_id', Unicode(32), ForeignKey('permissions.id'))
+            )
+
+
+class Permission(Base):
+    __tablename__ = 'permissions'
+    __repr__ = lambda self: "Permission(%s)" % (self.id, )
+    __str__ = lambda self: str(self.id)
+    __unicode__ = lambda self: self.id
+
+    id = Column(String(32), primary_key=True)
+    description = Column(Unicode(255))
+
+    groups = relation(Group, secondary=group_permissions, backref='_permissions')
 #def ready(sessionmaker):
 #    global session
 #    session = sessionmaker
