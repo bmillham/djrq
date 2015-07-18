92b8a7a809c18d9e4f5884c21a3ffbe2eee91bbf
Brian Millham <brian@millham.net>
2015-07-18 01:34:52 -0400

    Improved mistags page to highligh the reported mistag. Automatically deletes fixed mistags. Shows the filename of the reported mistag.

--- DJRQ/djrq/controllers/admin/mistagscontroller.py
+++ DJRQ/djrq/controllers/admin/mistagscontroller.py
@@ -5,6 +5,16 @@ from djrq.model import *
 class MistagsController(web.core.Controller):
     def __after__(self, *args, **kw):
         mistags = session.query(Mistags)
+        for m in mistags:
+            corrected = True
+            if m.song.artist.fullname != m.artist:
+                corrected = False
+            if m.song.album.fullname != m.album:
+                corrected = False
+            if m.song.title != m.title:
+                corrected = False
+            if corrected:
+                session.delete(m)
         return super(MistagsController, self).__after__(('djrq.templates.admin.mistags', 
                                                              dict(mistags=mistags)))
 
