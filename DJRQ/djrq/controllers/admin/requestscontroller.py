125364710949b344153f8c568630948b1b885ab2
Brian Millham <brian@millham.net>
2015-07-22 19:29:47 -0400

    Fixed bug then first accessing admin pages.

--- DJRQ/djrq/controllers/admin/requestscontroller.py
+++ DJRQ/djrq/controllers/admin/requestscontroller.py
@@ -11,12 +11,11 @@ class RequestsController(BaseController):
             change = None
         kw['change'] = change
         if 'request_view' not in web.core.session:
-            web.core.session['request_view'] = 'np'
+            web.core.session['request_view'] = ['np']
         return super(RequestsController, self).__before__(*args, **kw)
         
 
     def __after__(self, rkw, *args, **kw):
-        print rkw, args, kw
         if web.core.session['request_view'][0] == 'np':
             view = ['new', 'pending']
         else:
@@ -26,7 +25,6 @@ class RequestsController(BaseController):
         rkw['all_requests'] = dict()
         for stat in get_all_requests_info():
             print stat.status, stat.request_count, stat.request_length
-            #kw['all_requests'][stat.status] = dict()
             rkw['all_requests'][stat.status] = dict(request_count=stat.request_count, request_length=stat.request_length)
         empty = dict(request_count=0, request_length=0)
         for stat in ('new', 'pending', 'played', 'ignored'):
@@ -43,14 +41,10 @@ class RequestsController(BaseController):
 
     def change_status(self, *args, **kw):
         item, status = kw['change']
-        print "Change status to", status
         if status == 'delete':
-            #delmistag = session.query(Mistags).filter(Mistags.id==args[0]).one()
-            #session.autoflush = False
             session.delete(item)
             session.flush()
             kw['requests_count'], kw['requests_length'] = get_new_pending_requests_info()
-            #session.autoflush = True
         else:
             item.status = status
             session.add(item)
@@ -59,18 +53,3 @@ class RequestsController(BaseController):
     def change_view(self, *args, **kw):
         web.core.session['request_view'] = [args[0]]
         return kw
-
-    #def new(self, *args, **kw):
-    #    kw['rq_up'].status = 'new'
-    #    session.add(kw['rq_up'])
-    #    return kw
-
-    #def pending(self, *args, **kw):
-    #    kw['rq_up'].status = 'pending'
-    #    session.add(kw['rq_up'])
-    #    return kw
-
-    #def delete(self, *args, **kw):
-        #delmistag = session.query(Mistags).filter(Mistags.id==args[0]).one()
-        #session.delete(delmistag)
-    #    return kw
