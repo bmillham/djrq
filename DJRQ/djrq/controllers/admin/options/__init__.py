7dea9ac193f5b778e8de8cedba8217a24e75840c
Brian Millham <brian@millham.net>
2015-07-28 23:30:56 -0400

    Added ability to change show title and start time.

--- DJRQ/djrq/controllers/admin/options/__init__.py
+++ DJRQ/djrq/controllers/admin/options/__init__.py
@@ -1,5 +1,7 @@
 from ...basecontroller import BaseController
 from forms.catalogform import CatalogForm
+from forms.showinfoform import ShowInfoForm
 
 class OptionsController(BaseController):
     catalog = CatalogForm()
+    showinfo = ShowInfoForm()
---
+++ DJRQ/djrq/controllers/admin/options/forms/showinfoform.py
@@ -0,0 +1,34 @@
+import djrq.middleware
+import web
+from djrq.model import *
+
+class ShowInfoForm(web.core.HTTPMethod):
+    def __before__(self, *args, **kw):
+        options = session.query(SiteOptions).one()
+        kw['options'] = options
+        kw['show_title'] = options.show_title
+        kw['show_time'] = options.show_time
+        kw['show_end'] = options.show_end
+        return super(ShowInfoForm, self).__before__(*args, **kw)
+
+    def __after__(self, result, *args, **kw):
+        kw.update(result)
+        return super(ShowInfoForm, self).__after__(('djrq.templates.admin.options.forms.showinfo', kw))
+                                                             
+    def get(self, *args, **kw):
+        return dict(status=None)
+
+    def post(self, *args, **kw):
+        result = dict(status=None)
+        if 'showtitlefield' in kw:
+            if kw['options'].show_title != kw['showtitlefield']:
+                kw['options'].show_title = kw['showtitlefield']
+                result['show_title'] = kw['showtitlefield']
+                result['status'] = "[Updated]"
+        if 'showstartfield' in kw:
+            if kw['options'].show_time != kw['showstartfield']:
+                kw['options'].show_time = kw['showstartfield']
+                result['show_time'] = kw['showstartfield']
+                result['status'] = '[Updated]'
+        if result['status'] is not None: session.commit()
+        return result
